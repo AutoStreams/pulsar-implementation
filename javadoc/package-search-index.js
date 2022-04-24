a92917ab2d42849b8999b488d989f51ae204a62b
Lossar <Lossar@users.noreply.github.com>
2022-04-24 21:00:37 +0000

    Deploying to gh-pages from @ AutoStreams/pulsar-implementation@c7eda89bd196ad2a17ed3e25556550b79b04951d 🚀

--- javadoc/package-search-index.js
+++ javadoc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.autostreams.dataprovider"},{"l":"com.autostreams.pulsar"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.autostreams.datareceiver"},{"l":"com.autostreams.pulsar"}];updateSearchResults();
