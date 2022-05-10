8436b54695bf8bacf670748edc0c876d7ff079b1
Klungerbo <tomas.kolsen@gmail.com>
2022-05-10 02:30:46 +0200

    Cleanup dataprovider code

--- example/data-provider/src/main/java/com/autostreams/pulsar/dataprovider/DataProvider.java
+++ example/data-provider/src/main/java/com/autostreams/pulsar/dataprovider/DataProvider.java
@@ -5,6 +5,7 @@
 
 package com.autostreams.pulsar.dataprovider;
 
+import com.autostreams.utils.fileutils.FileUtils;
 import com.thedeanda.lorem.Lorem;
 import com.thedeanda.lorem.LoremIpsum;
 import io.netty.bootstrap.Bootstrap;
@@ -12,6 +13,7 @@ import io.netty.channel.ChannelFuture;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioSocketChannel;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,36 +25,95 @@ import org.slf4j.LoggerFactory;
  * @since 1.0
  */
 public final class DataProvider {
+    private static final String CONFIG_PROPERTIES_FILE_NAME = "config.properties";
+    private static final String HOST_PROPERTIES_VARIABLE_NAME = "producer.url";
+    private static final String PORT_PROPERTIES_VARIABLE_NAME = "producer.port";
+
+    private static final String HOST_ENVIRONMENT_VARIABLE_NAME = "PRODUCER_URL";
+    private static final String PORT_ENVIRONMENT_VARIABLE_NAME = "PRODUCER_PORT";
+
+    private String host = "127.0.0.1";
+    private Integer port = 8992;
+
     private final Logger logger = LoggerFactory.getLogger(DataProvider.class);
     private final Bootstrap bootstrap = new Bootstrap();
     private final Lorem lorem = LoremIpsum.getInstance();
-    private final String host;
-    private final int port;
     private EventLoopGroup group = new NioEventLoopGroup();
     private boolean running = true;
     private ChannelFuture channelFuture = null;
     private int messagesPerSecond = 1;
 
     /**
-     * Data provider constructor.
-     *
-     * @param host the host to connect to (URL)
-     * @param port the port to connect to
+     * Default constructors that uses environment or properties variables.
      */
-    public DataProvider(String host, int port) {
-        this.host = host;
-        this.port = port;
+    public DataProvider() {
+        setValues();
     }
 
     /**
-     * Creates a new data provider.
-     *
-     * @param host the host to connect to (URL)
-     * @param port the port to connect to
-     * @return the constructed data provider
+     * Set values from environment variables or properties.
+     * Note: Environment variables are prioritized over properties.
      */
-    public static DataProvider fromHostAndPort(String host, int port) {
-        return new DataProvider(host, port);
+    private void setValues() {
+        if (canSetValuesFromEnvironmentVariables()) {
+            setValuesFromEnvironmentVariables();
+        } else if (canSetValuesFromPropertiesFile()) {
+            setValuesFromPropertiesFile();
+        }
+    }
+
+    /**
+     * Checks if it is possible to set properties from environment variables.
+     *
+     * @return true if it is possible to set from configuration file, false if else.
+     */
+    private static boolean canSetValuesFromEnvironmentVariables() {
+        if (!System.getenv().containsKey(HOST_ENVIRONMENT_VARIABLE_NAME)) {
+            return false;
+        }
+
+        if (!System.getenv().containsKey(PORT_ENVIRONMENT_VARIABLE_NAME)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Set properties from environment variables.
+     */
+    private void setValuesFromEnvironmentVariables() {
+        host = System.getenv().get(HOST_ENVIRONMENT_VARIABLE_NAME);
+        port = Integer.parseInt(System.getenv().get(PORT_ENVIRONMENT_VARIABLE_NAME));
+    }
+
+    /**
+     * Checks if it is possible to set properties from configuration file.
+     *
+     * @return true if it is possible to set from configuration file, false if else.
+     */
+    private static boolean canSetValuesFromPropertiesFile() {
+        Properties props = FileUtils.loadPropertiesFromFile(CONFIG_PROPERTIES_FILE_NAME);
+
+        if (!props.containsKey(HOST_PROPERTIES_VARIABLE_NAME)) {
+            return false;
+        }
+
+        if (!props.containsKey(PORT_PROPERTIES_VARIABLE_NAME)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Set properties from configuration file.
+     */
+    private void setValuesFromPropertiesFile() {
+        Properties props = FileUtils.loadPropertiesFromFile(CONFIG_PROPERTIES_FILE_NAME);
+
+        host = props.getProperty(HOST_PROPERTIES_VARIABLE_NAME);
+        port = Integer.parseInt(props.getProperty(PORT_PROPERTIES_VARIABLE_NAME));
     }
 
     /**
@@ -61,58 +122,92 @@ public final class DataProvider {
      * @return true on success, false if else
      */
     public boolean initialize() {
+        setUpBootstrapGroup();
+
+        logger.info("Connecting to: {}:{}", host, port);
+        return tryToConnect();
+    }
+
+    /**
+     * Set up the bootstrap group.
+     */
+    private void setUpBootstrapGroup() {
         bootstrap.group(group)
             .channel(NioSocketChannel.class)
             .handler(new DataProducerInitializer(this));
+    }
 
-        int tries = 100;
-        int currentTry = 1;
-        int secondsToSleep = 5;
+    /**
+     * Tries to connect to the data receiver.
+     *
+     * @return true if successfully connected, false if else.
+     */
+    private boolean tryToConnect() {
         boolean connected = false;
-
-        logger.info("Connecting to: {}:{}", host, port);
-        while (!connected && currentTry <= tries) {
+        while (!connected) {
             try {
                 this.channelFuture = bootstrap.connect(host, port).sync();
                 connected = true;
             } catch (InterruptedException e) {
-                logger.warn("Connection was interrupted");
-                e.printStackTrace();
-                Thread.currentThread().interrupt();
+                handleConnectionInterruptedException(e);
 
                 return false;
             } catch (Exception e) {
-                e.printStackTrace();
-                logger.warn(
-                    "[{}/{}] Failed to initialize DataProducer, retrying in {} seconds",
-                    currentTry,
-                    tries,
-                    secondsToSleep
-                );
+                handleConnectionException(e);
             }
-
-            try {
-                TimeUnit.SECONDS.sleep(secondsToSleep);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-                Thread.currentThread().interrupt();
-
-                return false;
-            }
-
-            currentTry++;
         }
 
         return true;
     }
 
     /**
-     * Generate a random lorem ipsum string.
+     * Handle the connection Interrupted Exception.
      *
-     * @return a random lorem ipsum string of min <= n <= max amount of n words.
+     * @param e the exception to handle.
      */
-    private String getRandomString() {
-        return lorem.getWords(7, 12);
+    private void handleConnectionInterruptedException(InterruptedException e) {
+        logger.warn("Connection was interrupted");
+        e.printStackTrace();
+        Thread.currentThread().interrupt();
+    }
+
+    /**
+     * Handle the connection Exception.
+     *
+     * @param e the exception to handle.
+     */
+    private void handleConnectionException(Exception e) {
+        int secondsToSleep = 5;
+
+        e.printStackTrace();
+        logger.warn(
+            "Failed to initialize DataProducer, retrying in {} seconds",
+            secondsToSleep
+        );
+
+        sleepForSeconds(secondsToSleep);
+    }
+
+    /**
+     * Execute the DataProducer.
+     */
+    public void run() {
+        while (this.running) {
+            String line = getRandomString();
+            sendMessageToReceiver(line);
+            sleepForMilliseconds(1000 / messagesPerSecond);
+        }
+    }
+
+    /**
+     * Sends a message to the data receiver.
+     *
+     * @param message the message to send to the data receiver.
+     */
+    private void sendMessageToReceiver(String message) {
+        if (this.channelFuture != null) {
+            this.channelFuture = this.channelFuture.channel().writeAndFlush(message + "\r\n");
+        }
     }
 
     /**
@@ -130,43 +225,75 @@ public final class DataProvider {
     }
 
     /**
-     * Execute the DataProducer.
-     */
-    public void run() {
-        while (this.running) {
-            String line = getRandomString();
-            logger.trace("String created: {}", line);
-
-            if (this.channelFuture != null) {
-                this.channelFuture = this.channelFuture.channel().writeAndFlush(line + "\r\n");
-            }
-
-            try {
-                TimeUnit.MILLISECONDS.sleep(1000 / messagesPerSecond);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-                Thread.currentThread().interrupt();
-            }
-        }
-    }
-
-    /**
      * Shutdown the DataProducer.
      */
     public void shutdown() {
         this.running = false;
-        logger.info("Shutting down");
 
+        logger.info("Shutting down");
+        shutdownChannelFuture();
+        shutdownGroup();
+    }
+
+    /**
+     * Shutdown the event loop group.
+     */
+    private void shutdownGroup() {
+        if (group != null) {
+            logger.debug("Closing group");
+            group.shutdownGracefully();
+            group = null;
+        }
+    }
+
+    /**
+     * Shutdown the channel future.
+     */
+    private void shutdownChannelFuture() {
         if (channelFuture != null) {
             logger.debug("Closing channel future");
             channelFuture.channel().close();
             channelFuture = null;
         }
+    }
 
-        if (group != null) {
-            logger.debug("Closing group");
-            group.shutdownGracefully();
-            group = null;
+    /**
+     * Generate a random lorem ipsum string.
+     *
+     * @return a random lorem ipsum string of min <= n <= max amount of n words.
+     */
+    private String getRandomString() {
+        String line = lorem.getWords(7, 12);
+        logger.trace("String created: {}", line);
+
+        return line;
+    }
+
+    /**
+     * Sleep for a specified amount of seconds.
+     *
+     * @param seconds the amount of time to sleep in seconds.
+     */
+    private static void sleepForSeconds(int seconds) {
+        try {
+            TimeUnit.SECONDS.sleep(seconds);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    /**
+     * Sleep for a specified amount of milliseconds.
+     *
+     * @param milliseconds the amount of time to sleep in seconds.
+     */
+    private void sleepForMilliseconds(int milliseconds) {
+        try {
+            TimeUnit.MILLISECONDS.sleep(milliseconds);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            Thread.currentThread().interrupt();
         }
     }
 }
--- example/data-provider/src/main/java/com/autostreams/pulsar/dataprovider/Main.java
+++ example/data-provider/src/main/java/com/autostreams/pulsar/dataprovider/Main.java
@@ -1,8 +1,5 @@
 package com.autostreams.pulsar.dataprovider;
 
-import com.autostreams.utils.fileutils.FileUtils;
-import java.util.Properties;
-
 /**
  * The class containing the main entry point of the data producer application.
  *
@@ -10,15 +7,6 @@ import java.util.Properties;
  * @since 1.0
  */
 public class Main {
-    private static final String CONFIG_PROPERTIES_FILE_NAME = "config.properties";
-    private static final String HOST_PROPERTIES_VARIABLE_NAME = "producer.url";
-    private static final String PORT_PROPERTIES_VARIABLE_NAME = "producer.port";
-
-    private static final String HOST_ENVIRONMENT_VARIABLE_NAME = "PRODUCER_URL";
-    private static final String PORT_ENVIRONMENT_VARIABLE_NAME = "PRODUCER_PORT";
-
-    private static String host;
-    private static Integer port;
 
     /**
      * The main entry point of the application.
@@ -26,68 +14,10 @@ public class Main {
      * @param args the commandline arguments.
      */
     public static void main(String[] args) {
-        setValues();
-
-        DataProvider dataProvider = DataProvider.fromHostAndPort(host, port);
+        DataProvider dataProvider = new DataProvider();
         if (dataProvider.initialize()) {
+            dataProvider.setMessagesPerSecond(1);
             dataProvider.run();
         }
-    }
-
-    private static void setValues() {
-        if (canSetValuesFromEnvironmentVariables()) {
-            setValuesFromEnvironmentVariables();
-            return;
-        }
-
-        if (canSetValuesFromPropertiesFile()) {
-            setValuesFromPropertiesFile();
-            return;
-        }
-
-        setDefaultValues();
-    }
-
-    private static boolean canSetValuesFromEnvironmentVariables() {
-        if (!System.getenv().containsKey(HOST_ENVIRONMENT_VARIABLE_NAME)) {
-            return false;
-        }
-
-        if (!System.getenv().containsKey(PORT_ENVIRONMENT_VARIABLE_NAME)) {
-            return false;
-        }
-
-        return true;
-    }
-
-    private static void setValuesFromEnvironmentVariables() {
-        host = System.getenv().get(HOST_ENVIRONMENT_VARIABLE_NAME);
-        port = Integer.parseInt(System.getenv().get(PORT_ENVIRONMENT_VARIABLE_NAME));
-    }
-
-    private static boolean canSetValuesFromPropertiesFile() {
-        Properties props = FileUtils.loadPropertiesFromFile(CONFIG_PROPERTIES_FILE_NAME);
-
-        if (!props.containsKey(HOST_PROPERTIES_VARIABLE_NAME)) {
-            return false;
-        }
-
-        if (!props.containsKey(PORT_PROPERTIES_VARIABLE_NAME)) {
-            return false;
-        }
-
-        return true;
-    }
-
-    private static void setValuesFromPropertiesFile() {
-        Properties props = FileUtils.loadPropertiesFromFile(CONFIG_PROPERTIES_FILE_NAME);
-
-        host = props.getProperty(HOST_PROPERTIES_VARIABLE_NAME);
-        port = Integer.parseInt(props.getProperty(PORT_PROPERTIES_VARIABLE_NAME));
-    }
-
-    private static void setDefaultValues() {
-        host = "127.0.0.1";
-        port = 8992;
     }
 }
